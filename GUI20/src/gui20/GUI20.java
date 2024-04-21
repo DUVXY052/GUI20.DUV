7862568bb23a4f731d07165734d422489a365e19
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 19:35:32 -0500

    saturday

---
+++ GUI20/src/gui20/GUI20.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui20;
+
+import javax.swing.JFrame;
+import gui20.Dialog;
+
+
+public class GUI20 {
+
+    
+    public static void main(String[] args) {
+        Dialog dia= new Dialog ();
+        
+        
+        dia.setVisible(true);
+        dia.setLocation(null);
+    }
+    
+}
